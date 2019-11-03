eacfb22b53a64cecda5bd1c58f47a63d329e4004
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2019-11-03 19:44:22 +0530

    init commit

---
+++ src/main/java/com/spring/getready/GetreadyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.getready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetreadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetreadyApplication.class, args);
+	}
+
+}
