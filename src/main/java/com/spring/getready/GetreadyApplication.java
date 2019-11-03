a33edda43d12f865a844f7e26439c80d86349895
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2019-11-03 19:47:23 +0530

    Sample

--- src/main/java/com/spring/getready/GetreadyApplication.java
+++ src/main/java/com/spring/getready/GetreadyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GetreadyApplication {
 
 	public static void main(String[] args) {
+        System.out.print("Hello World!!!");
 		SpringApplication.run(GetreadyApplication.class, args);
 	}
 
